bbe050e8a192d162ff2a58298374e850b1e6864f
barikoi <barikoi@barikois-MacBook-Air.local>
2025-04-20 17:30:44 +0600

    fixed-bug

--- src/server.cpp
+++ src/server.cpp
@@ -54,8 +54,8 @@ void handle_client(int client, char **argv) {
         std::string content_encoding_value = breakdown_request(request,"Accept-Encoding:");
         std::cout<<content_encoding_value<<std::endl;
         std::string content = path.substr(6);
-        if (content_encoding_value.find("gzip") == 0){
-            message = "HTTP/1.1 200 OK\r\nContent-Encoding: "+content_encoding_value+ "\r\nContent-Type: text/plain\r\nContent-Length: " +
+        if (content_encoding_value.find("gzip")!= std::string::npos){
+            message = "HTTP/1.1 200 OK\r\nContent-Encoding: gzip\r\nContent-Type: text/plain\r\nContent-Length: " +
                       std::to_string(content.size()) + "\r\n\r\n" + content;
         }else{
             message = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " +
